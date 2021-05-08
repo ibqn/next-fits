5bd548d71a7e8e042f1aa9468ad1fa6585dabd0d
ibqn <evgen.ibqn@gmail.com>
2021-05-08 14:48:41 +0200

    add nprogress

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,17 @@
 import { ThemeProvider } from 'styled-components'
 
+import Router from 'next/router'
+import NProgress from 'nprogress'
+import '../components/styles/nprogress.css'
+
 import theme from '../theme'
 
 import GlobalStyle from '../components/global-style'
 import Page from '../components/page'
+
+Router.events.on('routeChangeStart', () => NProgress.start())
+Router.events.on('routeChangeComplete', () => NProgress.done())
+Router.events.on('routeChangeError', () => NProgress.done())
 
 export default function App({ Component, pageProps }) {
   return (
