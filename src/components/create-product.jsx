1feb2fb645efb311d29de9843c6604a2a1f5830f
ibqn <evgen.ibqn@gmail.com>
2021-05-22 19:22:11 +0200

    switch to formik components

--- src/components/create-product.jsx
+++ src/components/create-product.jsx
@@ -1,63 +1,57 @@
 import PropTypes from 'prop-types'
-import { useFormik } from 'formik'
+import { Formik, Field, ErrorMessage } from 'formik'
 import * as Yup from 'yup'
-import Form from './styles/form'
+import FormStyles from './styles/form'
+
+const PictureInput = ({ form, field }) => (
+  <input
+    name={field.name}
+    type="file"
+    accept="image/*"
+    onChange={({ target: { files } }) =>
+      form.setFieldValue(field.name, files[0])
+    }
+  />
+)
 
 const CreateProduct = () => {
-  const validate = (values) => {
-    const errors = {}
-
-    if (!values.name) {
-      errors.name = 'Required'
-    }
-
-    return errors
-  }
-
-  const validationSchema = Yup.object({
-    name: Yup.string().required('Product name is required'),
-    image: Yup.mixed()
-      .required('An image file is required')
-      .test('fileFormat', 'Image only', (value) => {
-        console.log('file', value)
-        console.log('type', value.type)
-        return value && ['image/jpeg', 'image/png'].includes(value.type)
-      }),
-  })
-
-  const formik = useFormik({
-    initialValues: {
-      name: 'New Product',
-      image: '',
-      price: 34642,
-      description: '',
-    },
-    validationSchema,
-    onSubmit: (values) => {
-      console.log(JSON.stringify(values, null, 2))
-    },
-  })
-
   return (
-    <Form onSubmit={formik.handleSubmit}>
-      <fieldset>
-        <label htmlFor="image">
-          Image
-          <input type="file" id="image" {...formik.getFieldProps('image')} />
-          {formik.touched.image && formik.errors.image ? (
-            <div>{formik.errors.image}</div>
-          ) : null}
-        </label>
-        <label htmlFor="name">
-          Product Name
-          <input id="name" type="text" {...formik.getFieldProps('name')} />
-          {formik.touched.name && formik.errors.name ? (
-            <div>{formik.errors.name}</div>
-          ) : null}
-        </label>
-        <button type="submit">+ Add Product</button>
-      </fieldset>
-    </Form>
+    <Formik
+      initialValues={{
+        name: 'New Product',
+        image: '',
+        price: 34642,
+        description: '',
+      }}
+      validationSchema={Yup.object({
+        name: Yup.string().required('Product name is required'),
+        image: Yup.mixed()
+          .required('An image file is required')
+          .test('fileFormat', 'Image only', (value) => {
+            console.log('file', value)
+            return value && ['image/jpeg', 'image/png'].includes(value.type)
+          }),
+      })}
+      onSubmit={(values) => console.log(values)}
+    >
+      {() => (
+        <FormStyles>
+          <fieldset>
+            <label htmlFor="image">
+              Product Name
+              <Field name="image" component={PictureInput} />
+              <ErrorMessage name="image" />
+            </label>
+            <label htmlFor="name">
+              Product Name
+              <Field name="name" />
+              <ErrorMessage name="name" />
+            </label>
+            <button type="submit">+ Add Product</button>
+          </fieldset>
+        </FormStyles>
+      )}
+    </Formik>
   )
 }
 
